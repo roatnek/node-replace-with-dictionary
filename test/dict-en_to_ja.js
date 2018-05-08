ffe74851744e20e5cdcdc8fe9e0a0654ca851b1b
Kentaro Tanaka <tnkktr@gmail.com>
2018-05-08 17:07:05 +0900

    Add test files

---
+++ test/dict-en_to_ja.js
@@ -0,0 +1,7 @@
+module.exports = [
+  ['(<html.*)lang=".*"(.*>)', '$1lang="ja"$2'],
+  ['Fruit', '果物'],
+  ['apple', 'りんご'],
+  ['orange', 'みかん'],
+  ['cherry', 'さくらんぼ'],
+];
---
+++ test/dict-ja_to_en.js
@@ -0,0 +1,7 @@
+module.exports = [
+  ['(<html.*)lang=".*"(.*>)', '$1lang="en"$2'],
+  ['果物', 'Fruit'],
+  ['りんご', 'apple'],
+  ['みかん', 'orange'],
+  ['さくらんぼ', 'cherry'],
+];
